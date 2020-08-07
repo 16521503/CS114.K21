38f07fa34af11c6d26a12c6535f9674b2bb21fe2
hatk <ha.trankhanh@hoozing.onmicrosoft.com>
2020-08-07 09:27:27 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# CS114.K21
+# Installing Packages
+Using `conda`:
+```
+(venv) $ conda install --file requirements.txt
+```
+
+Using `pip`:
+```
+(venv) $ pip install -r requirements.txt
+```
+
+# Dataset information
+```
+ - Dataset will be save in images folder, which contain 2 folder, train and test folder.
+ - For each folder will be devide two folder, motorbike and non-motorbike for train and test.
+```
+
+# Train model(step to step)
+```
+ - Director to dataset defined in constants.py file.
+ - Run file train.py to train model and save it in project. 
+ - Model will be save as model.sav file.
+```
+
+# Test model(step to step)
+```
+ - Run file test.py to test model.
+ - Model was saved when trained in the last step, will be load to run test. Dont need to train model again.
+ - Result of test will be print in compiler run time.
+```
---
+++ test.py
@@ -0,0 +1,52 @@
+from constants import DIR_MODEL, DIR_TEST, LABELS
+from sklearn.metrics import accuracy_score
+import numpy as np
+import pickle
+import utils
+import vgg16
+
+def test():
+    X_test, y_test, paths = utils.read_process_images(DIR_TEST, LABELS, return_paths=True)
+    print('X_test shape: %s' % str(X_test.shape))
+    print('y_test shape: %s' % str(y_test.shape))
+
+    X_test_fc6 = vgg16.extract_fc6_features(X_test, verbose=True)
+    print('X_test_fc6 shape: %s' % str(X_test_fc6.shape))
+
+    # load model from disk
+    loaded_model = pickle.load(open(DIR_MODEL, 'rb'))
+
+    # predict at test data
+    y_pred = loaded_model.predict(X_test_fc6)
+
+    print('Y test = ')  
+    print(y_test)
+    print('Y predict = ')
+    print(y_pred)
+
+    scores = loaded_model.score(X_test_fc6, y_test)
+    print("accuracy = %f" % scores)
+
+    # Print false predictions
+    diff = y_test != y_pred
+    if np.any(diff):
+        print('false predictions:')
+        print(paths[diff])
+
+    # Print confidence score of each sample
+    print('confidence scores:')
+    print(loaded_model.decision_function(X_test_fc6))
+
+    # Precision and Recall for motobike - 0
+    tp, fp, fn = 0, 0, 0
+    for p, a in zip(y_test, y_pred):
+        tp += ((a == 0) and (p == 0))
+        fn += ((a == 0) and (p == 1))
+        fp += ((a == 1) and (p == 0))
+    precision = tp / max(1, (tp + fp))
+    recall = tp / max(1, (tp + fn))
+    f1_score = (2 * precision * recall) / (precision + recall)
+    print("Precision: {0:.3}\nRecall: {1:.3}\nF1-score: {2:.3}".format(precision, recall, f1_score))
+    print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
+if __name__ == '__main__':
+    test()
